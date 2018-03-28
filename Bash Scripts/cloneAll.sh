95526a1154dd6fb215e06459cbc224990a13bcad
awtorode@gmail.com <awtorode@gmail.com>
2018-03-28 13:01:16 +0100

    Added cloneAll script

---
+++ Bash Scripts/cloneAll.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone https://github.com/torodean/Antonius-Handbook.git
+git clone https://github.com/torodean/Antonius-Personal.git
+git clone https://github.com/torodean/Antonius-MIA.git
+git clone https://github.com/torodean/Antonius-Websites.git
+git clone https://github.com/torodean/Antonius-Templates.git
+git clone https://github.com/torodean/Antonius-GINA.git
+git clone https://github.com/torodean/Antonius-Cookbook.git
+git clone https://github.com/torodean/Antonius-Notes.git
+git clone https://github.com/torodean/The-Potato-Pages.git
+
