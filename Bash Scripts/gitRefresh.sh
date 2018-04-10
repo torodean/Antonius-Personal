25d667a902490d96fac7560ad21462bd685c0e43
awtorode@gmail.com <awtorode@gmail.com>
2018-04-10 18:55:37 +0100

    Updated gitRefresh script

--- Bash Scripts/gitRefresh.sh
+++ Bash Scripts/gitRefresh.sh
@@ -1,33 +1,63 @@
 #!/bin/sh
+echo "...Updating repository for Antonius-Handbook"
 cd Antonius-Handbook
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-Websites"
 cd Antonius-Websites
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-Notes"
 cd Antonius-Notes
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-Personal"
 cd Antonius-Personal
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-MIA"
 cd Antonius-MIA
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-Templates"
 cd Antonius-Templates
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Antonius-GINA"
 cd Antonius-GINA
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for The-Potato-Pages"
 cd The-Potato-Pages
 git pull
 git status
 cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "...Updating repository for Project--ALIAS"
+cd Project--ALIAS
+git pull
+git status
+cd ..
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
