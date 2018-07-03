1f125a5a05c37ab276ff0e35536e093488af931f
Antonius W. Torode <torodean@pa.msu.edu.intranet>
2018-07-03 18:00:21 +0100

    Modified gscraper

--- Webscraping/WoW Guild scraper/gscraper.py
+++ Webscraping/WoW Guild scraper/gscraper.py
@@ -26,9 +26,9 @@ memberAchievementPoints = []
 
 def returnVar(input):
 	'''Takes a string of the form "var: val" and returns var.'''
-	return input.split(':')[0])
+	return input.split(':')[0]
 	
-def returnVal(input)
+def returnVal(input):
 	'''Takes a string of the form "var: val" and returns val.'''
 	return input.split(':')[1].lstrip()
 
@@ -51,6 +51,16 @@ def find_between( s, first, last ):
 		return ""
 
 names = []
+realm = []
+classes = ['guild']
+race = ['guild']
+gender = ['guild']
+level = []
+achievementPoints = []
+spec = ['guild']
+description = ['guild']
+rank = ['guild']
+memberCounter = 0
 clean_lines = []
 
 with file_orig as f:
@@ -61,8 +71,64 @@ with file_orig as f:
 	clean_lines = [l.replace(",","") for l in clean_lines if l.replace(",","")]
 	clean_lines = [l.replace("\"","") for l in clean_lines if l.replace("\"","")]
 	clean_lines = [l.replace(" \"","\"") for l in clean_lines if l.replace(" \"","\"")]
+	clean_lines = ["---" if "character" in x else x for x in clean_lines]
+	clean_lines = ["" if "lastModified" in x else x for x in clean_lines]
+	clean_lines = ["" if "backgroundImage" in x else x for x in clean_lines]	
+	clean_lines = ["" if "thumbnail" in x else x for x in clean_lines]
+	clean_lines = ["" if "icon" in x else x for x in clean_lines]
+	clean_lines = ["" if "battlegroup" in x else x for x in clean_lines]
+	clean_lines = ["" if "role" in x else x for x in clean_lines]
+	clean_lines = ["" if "order" in x else x for x in clean_lines]
+	clean_lines = ["" if "guild" in x else x for x in clean_lines]
+	clean_lines = ["spec2" if "spec" in x else x for x in clean_lines]
+	clean_lines = [l.replace("","") for l in clean_lines if l.replace("","")]
 	
+for i, line in enumerate(clean_lines):
+	if "spec" in line:
+		clean_lines[i+1] = clean_lines[i+1].replace("name","spec")
+
+clean_lines = [l.replace("spec2","") for l in clean_lines if l.replace("spec2","")]
+
+for line in clean_lines:
+	if "name" in line:
+		memberCounter += 1 
+		names.append(line.split(':')[1].lstrip())
+	if "realm" in line:
+		realm.append(line.split(':')[1].lstrip())
+	if "class" in line:
+		classes.append(line.split(':')[1].lstrip())
+	if "race" in line:
+		race.append(line.split(':')[1].lstrip())
+	if "gender" in line:
+		gender.append(line.split(':')[1].lstrip())
+	if "level" in line:
+		level.append(line.split(':')[1].lstrip())
+	if "achievementPoints" in line:
+		achievementPoints.append(line.split(':')[1].lstrip())
+	if "spec" in line:
+		spec.append(line.split(':')[1].lstrip())
+	if "description" in line:
+		description.append(line.split(':')[1].lstrip())
+	if "rank" in line: 
+		rank.append(line.split(':')[1].lstrip())
+		
 with file_modified as f:
 	f.writelines('\n'.join(clean_lines))
 	
-print(names)
+
+print(len(names))
+print(len(level))
+print(len(rank))
+	
+print('Name\t\tlevel\tspec\trank\t')
+for i, val in enumerate(names):
+	print('{0}\t\t{1}\t{2}'.format(names[i],level[i],rank[i]))
+	
+print('\nTotal members : {0}'.format(memberCounter))
+
+
+
+
+
+
+
