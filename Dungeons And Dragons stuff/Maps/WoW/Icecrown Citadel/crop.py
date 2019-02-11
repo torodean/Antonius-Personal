419c7a0a92929fdd64d6e108bb0b0094321044dd
awtorode@gmail.com <awtorode@gmail.com>
2019-02-11 19:49:57 +0000

    cropped WoW images

---
+++ Dungeons And Dragons stuff/Maps/WoW/Icecrown Citadel/crop.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+def crop(image_path, coords, saved_location):
+    """
+    @param image_path: The path to the image to edit
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path to save the cropped image
+    """
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(saved_location)
+    cropped_image.show()
+
+num=4812
+	
+if __name__ == '__main__':
+    for imgnumber in range(1,17):
+        image = '{0}-{1}.jpg'.format(num,imgnumber)
+        crop(image, (0, 0, 752, 506), 'cropped-{0}-{1}.jpg'.format(num,imgnumber))
---
+++ Dungeons And Dragons stuff/Maps/WoW/Karazhan/crop.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def crop(image_path, coords, saved_location):
+    """
+    @param image_path: The path to the image to edit
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path to save the cropped image
+    """
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(saved_location)
+    cropped_image.show()
+ 
+ 
+if __name__ == '__main__':
+    for imgnumber in range(1,17):
+        image = '3457-{0}.jpg'.format(imgnumber)
+        crop(image, (0, 0, 752, 506), 'cropped-3457-{0}.jpg'.format(imgnumber))
