a6be32c97597922a66ba3658575522d3508c2b68
awtorode@gmail.com <awtorode@gmail.com>
2019-02-27 17:00:01 +0000

    Added script for copying TS pdf

---
+++ copyAsPrintable.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "...Loading."
+echo "...Created by Antonius Torode."
+echo "...Saving printable version of TS."
+
+cp -v "Dungeons And Dragons stuff\Campaigns\Trinity Stones\Trinity Stones.pdf" "Dungeons And Dragons stuff\Campaigns\Trinity Stones\Trinity Stones-printable.pdf"
