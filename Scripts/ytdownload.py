957d9c497232d47479defaa6fb479440de410227
awtorode@gmail.com <awtorode@gmail.com>
2019-03-18 17:01:47 +0100

    Fixed commit issues

---
+++ Scripts/ytdownload.py
@@ -0,0 +1,30 @@
+##################################################################################
+## Program: ytdownload.py
+## Author: Antonius Torode
+## Date: 3/13/2019
+## Purpose: For downloading videos from youtube then converting to mp3.
+##################################################################################
+
+from pytube import YouTube
+import moviepy.editor as mp
+
+urlinput = "https://www.youtube.com/watch?v=MoHCpa4rG7I&t=9s"
+
+print('...Processing Youtube download for {0}'.format(urlinput))
+yt = YouTube(urlinput)
+title = yt.title
+
+print('...Available streams are as follows:')
+print(yt.streams.all())
+stream = yt.streams.first()
+print('...Downloading first stream ({0}):'.format(title))
+stream.download('./ytdownloads')
+
+print('...Finished download.')
+print('...Starting converter.')
+
+clip = mp.VideoFileClip('./ytdownloads/{0}.mp4'.format(title))
+clip.audio.write_audiofile('./ytdownloads/{0}.mp3'.format(title))
+
+print('...Finished converter.')
+
