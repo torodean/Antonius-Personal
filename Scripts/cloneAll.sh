7de42c8034e2c9dfcd05f12556dca904b5958abc
Antonius Torode <24546058+torodean@users.noreply.github.com>
2021-02-23 21:33:12 -0600

    Updated cloneAll to include new projects.

--- Scripts/cloneAll.sh
+++ Scripts/cloneAll.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+File Edit Options Buffers Tools Sh-Script Help                                                                         
+#!/bin/sh                                                                                                              
 
 git clone https://github.com/torodean/Antonius-Handbook.git
 git clone https://github.com/torodean/Antonius-Handbook-II.git
@@ -10,4 +11,11 @@ git clone https://github.com/torodean/Antonius-GINA.git
 git clone https://github.com/torodean/Antonius-Cookbook.git
 git clone https://github.com/torodean/Antonius-Notes.git
 git clone https://github.com/torodean/The-Potato-Pages.git
+git clone https://github.com/torodean/torodean.github.io.git
+git clone https://github.com/torodean/Antonius-InventoryManagement.git
+git clone https://github.com/torodean/Antonius-GameTemplate
+
+git clone https://github.com/GitWebsiteTutorial/GitWebsiteTutorial.github.io.git
+git clone https://github.com/MMORPDND/MMORPDND.github.io.git
+
 
