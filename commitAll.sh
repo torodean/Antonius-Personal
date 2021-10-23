2bece8536311ddc52fb7d33dcca92040e849c2f6
unknown <awtorode@gmail.com>
2021-10-23 02:26:46 +0100

    Added commitAll script

---
+++ commitAll.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "...Loading."
+echo "...Created by Antonius Torode."
+echo "...Please make sure you are connected to the Internet."
+echo "...If this script isn't working, make sure EOL Conversion is set to Unix."
+echo "..."
+git status
+echo "..."
+git add -A
+echo "..."
+git status
+echo "..."
+git commit -m "$1"
+echo "..."
+git status
+echo "..."
+git push
+echo "..."
+echo "...Finished."
