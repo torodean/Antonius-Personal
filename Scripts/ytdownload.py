9a2b5347d8131b0ca783aeb4cb26206f464cb582
unknown <awtorode@gmail.com>
2021-12-10 22:19:59 +0000

    -m

--- Scripts/ytdownload.py
+++ Scripts/ytdownload.py
@@ -7,6 +7,7 @@
 
 from pytube import YouTube
 from os import path
+from os import system
 import argparse
 import moviepy.editor as mp
 import sys
@@ -27,79 +28,101 @@ downloadDirectory = 'ytdownloads'
 
 
 def fileExists(fileName, subDirectory = '', fileExtension = '.txt'):
-	"""
-	Checks if a file exists.
-	"""
-	if subDirectory == '':
-		file = fileName
-	else:
-		file = subDirectory + '/' + fileName + '.' + fileExtension
-	print("...checking if {0} exists.".format(file))
-	sys.stdout.flush()
-	return path.exists(file)
-	
+    """
+    Checks if a file exists.
+    """
+    if subDirectory == '':
+        file = fileName
+    else:
+        file = subDirectory + '/' + fileName + '.' + fileExtension
+    print("...checking if {0} exists.".format(file))
+    sys.stdout.flush()
+    return path.exists(file)
+    
+
+def extractHighestBitRateAsitag(list):
+    bitrate = 0
+    itag = "0"
+    for item in list:
+        br = item.split("abr=\"")[1].split("kbp")[0]
+        if int(br) > bitrate:
+            bitrate = int(br)
+    for item in list:
+        if "abr=\"{0}".format(bitrate) in item:
+            itag = item.split("itag=\"")[1].split("\" ")[0]
+    return int(itag)
+
 
 def downloadUrl(url):
-	"""
-	Downloads a specified URL file.
-	"""
-	try: 
-		print('...Processing Youtube download for {0}'.format(url))
-		sys.stdout.flush()
-		yt = YouTube(url)
-		title = yt.title
-		
-		if fileExists(title, downloadDirectory, 'mp4'):
-			print("...File already exists: {0}/{1}.mp4".format(downloadDirectory, title))
-			print('...Finished download.')
-		else:
-			if verboseMode:
-				print('...Available streams are as follows:')
-				print(yt.streams.all())
-				sys.stdout.flush()
-			stream = yt.streams.first()
-			print('...Downloading first stream ({0}):'.format(title))
-			sys.stdout.flush()
-			stream.download(downloadDirectory)
+    """
+    Downloads a specified URL file.
+    """
+    try: 
+        print('...Processing Youtube download for {0}'.format(url))
+        sys.stdout.flush()
+        yt = YouTube(url)
+        title = yt.title
+        fileName = ''.join(e for e in title if e.isalnum() or e.isspace())
+        print(fileName)
+        
+        if fileExists(fileName, downloadDirectory, 'mp4'):
+            print("...File already exists: {0}/{1}.mp4".format(downloadDirectory, fileName))
+            print('...Finished download.')
+        else:
+            streams = yt.streams.filter(only_audio=True, file_extension='mp4')
+            streamlist = []
+            for stream in streams:
+                print(stream)
+                streamlist.append(str(stream))
+            sys.stdout.flush()
+            itag = extractHighestBitRateAsitag(streamlist)
+            stream = yt.streams.get_by_itag(itag)
+            print('...Downloading first stream ({0}):'.format(title))
+            sys.stdout.flush()
+            stream.download(downloadDirectory)
+            system("cp -uv \"{0}/\"*\".mp4\" \"{0}/{1}.mp4\"".format(downloadDirectory, fileName))
 
-			print('...Finished download.')
-			convertMp4(title)
-	except Exception as ex:
-		print(ex)
-		print("...An error occured when downloading the specified URL.")
+            print('...Finished download.')
+        convertMp4(fileName)
+        deleteMp4s()
+    except Exception as ex:
+        print(ex)
+        print("...An error occured when downloading the specified URL.")
 
 def convertMp4(title):
-	"""
-	Converts an mp4 file to mp3.
-	"""
-	try: 
-		if fileExists(title, downloadDirectory, 'mp3'):
-			print("...File already exists: {0}/{1}.mp3".format(downloadDirectory, title))
-			print('...Finished converter.')
-		else:
-			print('...Starting converter for {0}.'.format(title))
-			clip = mp.VideoFileClip('{0}/{1}.mp4'.format(downloadDirectory, title))
-			clip.audio.write_audiofile('{0}/{1}.mp3'.format(downloadDirectory, title))
-			
-			print('...Finished converter.')
-		
-	except Exception as ex:
-		print(ex)
-		print("...An error occured when Converting the specified URL.")
+    """
+    Converts an mp4 file to mp3.
+    """
+    try: 
+        #if fileExists(title, downloadDirectory, 'mp3'):
+        #    print("...File already exists: {0}/{1}.mp3".format(downloadDirectory, title))
+        #    print('...Finished converter.')
+        #else:
+        print('...Starting converter for {0}.mp4'.format(title))
+        clip = mp.AudioFileClip('{0}/{1}.mp4'.format(downloadDirectory, title))
+        clip.write_audiofile('{0}/{1}.mp3'.format(downloadDirectory, title))
+        
+        print('...Finished converter.')
+        
+    except Exception as ex:
+        print(ex)
+        print("...An error occured when Converting the specified URL.")
 
 
 def deleteMp4s():
-	"""
-	Used for deleting mp4 files after converting.
-	"""
-	
+    """
+    Used for deleting mp4 files after converting.
+    """
+    print("...Deleting mp4's")
+    system("rm -vrf {0}/*.mp4".format(downloadDirectory))
+    
 
 if urlinput != 'None' and listInput == 'None':
-	downloadUrl(urlinput)
+    downloadUrl(urlinput)
 elif listInput != 'None' and urlInput == 'None':
-	downloadList
+    downloadList
 else:
-	print("...Nothing to convert, invalid inputs.")
+    print("...Nothing to convert, invalid inputs.")
 
 print('...Finished program.')
 
