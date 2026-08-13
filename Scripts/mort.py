7fcef12a3c7bb60a98802b5aa53f506d70200720
Antonius Torode <awtorode@gmail.com>
2026-08-13 18:35:55 -0500

    Improving mort

--- Scripts/mort.py
+++ Scripts/mort.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # SETTINGS
 # ============================================================
 
-YEARS = 30
+YEARS = 5
 MONTHS = YEARS * 12
 monthly_income = 7451.99 + 3900
 monthly_income = monthly_income * 0.8
@@ -40,10 +40,11 @@ rent_misc = [
 # Mortgage
 # -----------------------
 
-house_price = 375000
+house_price = 350000
 down_payment = 200000
 closing_costs = 20000
 renting_income = 0
+monthly_house_taxes = 750
 
 annual_interest = 0.0699
 loan_years = 30
@@ -61,7 +62,6 @@ mortgage_misc = [
     150,   # car insurance
     400,   # rental insurance
    1000,   # misc spending
-    750,   # taxes
     210*2/12, # phone
 ]
 
@@ -138,15 +138,17 @@ remaining_balance = loan
 
 # Values to plot
 mortgage_interest_paid = []
+waste_to_mortgage = [] # amount of money 'wasted' to interest and taxes similar to how renting is 'waste'
 mortgage_equity = []
 mortgage_savings = []
 
 # Running totals
 total_interest_paid = mandatory_points
+total_taxes_paid = 0
 total_equity = down_payment      # Starts with the down payment
 total_savings = emergency_fund   # Start with emergency fund.
 
-monthly_expenses = sum(mortgage_misc)
+monthly_expenses = sum(mortgage_misc) + monthly_house_taxes
 
 for month in range(MONTHS):
 
@@ -172,6 +174,8 @@ for month in range(MONTHS):
         total_savings += monthly_savings + renting_income
 
         mortgage_interest_paid.append(total_interest_paid)
+        total_taxes_paid += monthly_house_taxes
+        waste_to_mortgage.append(total_interest_paid + total_taxes_paid)
         mortgage_equity.append(total_equity)
         mortgage_savings.append(total_savings)
 
@@ -207,6 +211,7 @@ for month in range(MONTHS):
 
     # Update cumulative totals
     total_interest_paid += monthly_interest
+    total_taxes_paid += monthly_house_taxes
     total_equity += principal_payment
 
     # Money left over after all expenses
@@ -228,6 +233,7 @@ for month in range(MONTHS):
 
     # Store values for plotting
     mortgage_interest_paid.append(total_interest_paid)
+    waste_to_mortgage.append(total_interest_paid + total_taxes_paid)
     mortgage_equity.append(total_equity)
     mortgage_savings.append(total_savings)
     
@@ -240,6 +246,9 @@ for month in range(MONTHS):
             monthly_savings,
             total_savings,
         )
+        
+    if month == 60:
+        break
 
 # ============================================================
 # Plot
@@ -249,50 +258,10 @@ years = [m / 12 for m in range(MONTHS)]
 
 plt.figure(figsize=(12,7))
 
-plt.plot(years, 
-         rent_waste, 
-         label="Rent Waste", 
-         linestyle="dotted")
-plt.plot(years, 
-         rent_savings, 
-         label="Rent Savings", 
-         linestyle="--")
-
-plt.plot(years, 
-         mortgage_interest_paid,
-         label="Mortgage Interest (Waste)", 
-         linestyle="dotted")
-
-plt.plot(years, 
-         mortgage_savings,
-         label="Mortgage Savings",
-         linestyle="--")
-
-plt.plot(years, 
-         mortgage_equity,
-         label="Home Equity")
-
 net_worth = [
     savings + equity
     for savings, equity in zip(mortgage_savings, mortgage_equity)
 ]
-
-plt.plot(years, 
-         net_worth,
-         label="Net Worth")
-
-plt.plot(years, 
-         mortgage_equity,
-         label="Home Equity")
-
-plt.xlabel("Years")
-plt.ylabel("Dollars")
-plt.title("Renting vs Mortgage")
-plt.grid(True)
-plt.legend()
-
-plt.tight_layout()
-plt.show()
 
 # ============================================================
 # Summary
@@ -306,4 +275,24 @@ print()
 
 print("======== Mortgage ========")
 print(f"Interest Paid:        ${total_interest_paid:,.2f}")
+print(f"Total Waste:          ${total_interest_paid + total_taxes_paid:,.2f}")
 print(f"Savings:              ${total_savings:,.2f}")
+
+plt.plot(years, rent_waste, label="Rent Waste", linestyle="dotted")
+plt.plot(years, rent_savings, label="Rent Savings", linestyle="--")
+plt.plot(years, waste_to_mortgage, label="Mortgage (Waste)", linestyle="dotted")
+plt.plot(years, mortgage_savings, label="Mortgage Savings", linestyle="--")
+plt.plot(years, mortgage_equity, label="Home Equity")
+plt.plot(years, net_worth, label="Net Worth")
+plt.plot(years, mortgage_equity, label="Home Equity")
+
+plt.xlabel("Years")
+plt.ylabel("Dollars")
+plt.title("Renting vs Mortgage")
+plt.grid(True)
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
